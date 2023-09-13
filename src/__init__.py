d7af0429ed19ff61d72debf4852359688e955287
alexpantyukhin <apantykhin@gmail.com>
2023-09-13 11:36:32 +0400

    code formating

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,3 @@
-
 from .support_resistance import get_support_resistance_levels, LevelCharacteristics
 
-__all__ = [
-    'get_support_resistance_levels',
-    'LevelCharacteristics'
-]
+__all__ = ["get_support_resistance_levels", "LevelCharacteristics"]
--- src/support_resistance.py
+++ src/support_resistance.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from functools import lru_cache
 from typing import Optional, Callable, List, Tuple
 
+
 # Class to store level characteristics
 @dataclass
 class LevelCharacteristics:
@@ -14,22 +15,29 @@ class LevelCharacteristics:
     percent_abs_value: float
     level_value: float
 
-# Function to find extrema
-def _find_extremas(stock_prices: pd.DataFrame, order: int
-                   ) -> Tuple[List[int], List[int], List[int], List[int]]:
 
-    min_open_close = stock_prices[['close', 'open']].min(axis=1).to_numpy()
-    max_open_close = stock_prices[['close', 'open']].max(axis=1).to_numpy()
+# Function to find extrema
+def _find_extremas(
+    stock_prices: pd.DataFrame, order: int
+) -> Tuple[List[int], List[int], List[int], List[int]]:
+    min_open_close = stock_prices[["close", "open"]].min(axis=1).to_numpy()
+    max_open_close = stock_prices[["close", "open"]].max(axis=1).to_numpy()
 
     min_extrema = argrelextrema(min_open_close, np.less_equal, order=order)
     max_extrema = argrelextrema(max_open_close, np.greater_equal, order=order)
 
-    return min_extrema[0], max_extrema[0], list(min_open_close[min_extrema[0]]), list(max_open_close[max_extrema[0]])
+    return (
+        min_extrema[0],
+        max_extrema[0],
+        list(min_open_close[min_extrema[0]]),
+        list(max_open_close[max_extrema[0]]),
+    )
+
 
 # Function to find left and right indexes within delta range
-def _find_left_right_indexes_in_delta(sorted_array: List[float],
-                                      delta_absolute: float
-                                      ) -> List[Tuple[int, int]]:
+def _find_left_right_indexes_in_delta(
+    sorted_array: List[float], delta_absolute: float
+) -> List[Tuple[int, int]]:
     left_bound = 0
     right_bound = 0
     len_sorted_array = len(sorted_array)
@@ -54,6 +62,7 @@ def _find_left_right_indexes_in_delta(sorted_array: List[float],
 
     return result
 
+
 # Function to get indexes of values
 def _get_value_indexes(keys, values):
     dct = {}
@@ -65,17 +74,16 @@ def _get_value_indexes(keys, values):
 
     return dct
 
-# Function to get level characteristics
-def _get_level_characteristics(stocks: pd.DataFrame,
-                               order: int,
-                               percentage: int
-                               ) -> List[LevelCharacteristics]:
 
+# Function to get level characteristics
+def _get_level_characteristics(
+    stocks: pd.DataFrame, order: int, percentage: int
+) -> List[LevelCharacteristics]:
     mins, maxs, mins_vals, maxs_vals = _find_extremas(stocks, order)
     extremas = sorted(mins_vals + maxs_vals)
     len_extremas = len(extremas)
 
-    max_value = stocks[['close', 'low', 'high', 'open']].max(axis=1).to_numpy().max()
+    max_value = stocks[["close", "low", "high", "open"]].max(axis=1).to_numpy().max()
     percent_abs_value = max_value * percentage / 100
     left_right_indexes = _find_left_right_indexes_in_delta(extremas, percent_abs_value)
 
@@ -98,17 +106,23 @@ def _get_level_characteristics(stocks: pd.DataFrame,
             if extrema in maxs_values_indexes:
                 maxs_full += maxs_values_indexes[extrema]
 
-        result.append(LevelCharacteristics(mins_indexes=mins_full,
-                                           maxs_indexes=maxs_full,
-                                           delta_spread_percentage=percentage,
-                                           percent_abs_value=percent_abs_value,
-                                           level_value=level))
+        result.append(
+            LevelCharacteristics(
+                mins_indexes=mins_full,
+                maxs_indexes=maxs_full,
+                delta_spread_percentage=percentage,
+                percent_abs_value=percent_abs_value,
+                level_value=level,
+            )
+        )
 
     return result
+
 
 # Function to get the number of points on a level
 def _get_level_points(level: LevelCharacteristics) -> int:
     return len(level.mins_indexes) + len(level.maxs_indexes)
+
 
 # Function for binary search
 def _binary_search(low, high, arr, x):
@@ -125,11 +139,13 @@ def _binary_search(low, high, arr, x):
 
     return len(arr)
 
+
 # Function to merge levels
-def _merge_levels_best_way(levels: List[LevelCharacteristics],
-                           max_levels: Optional[int] = None,
-                           level_point: Callable[[LevelCharacteristics], float] = None
-                           ) -> Tuple[int, List[LevelCharacteristics]]:
+def _merge_levels_best_way(
+    levels: List[LevelCharacteristics],
+    max_levels: Optional[int] = None,
+    level_point: Callable[[LevelCharacteristics], float] = None,
+) -> Tuple[int, List[LevelCharacteristics]]:
     len_levels = len(levels)
     levels_values = list(map(lambda x: x.level_value, levels))
     if level_point is None:
@@ -142,23 +158,29 @@ def _merge_levels_best_way(levels: List[LevelCharacteristics],
 
         level = levels[index]
         next_level_index = _binary_search(
-            index, len_levels - 1,
+            index,
+            len_levels - 1,
             levels_values,
-            level.level_value + level.percent_abs_value)
+            level.level_value + level.percent_abs_value,
+        )
 
         # 1. we skip the current level and find the best variant without the current one.
         without_curr_level_points, without_curr_level_result = get_best_dp(
-            index + 1, left_levels)
+            index + 1, left_levels
+        )
 
         # 2. we check the current level and find the best variant with the current one.
         next_left_levels = None if left_levels is None else left_levels - 1
         with_curr_level_point, with_curr_level_result = get_best_dp(
-            next_level_index,
-            next_left_levels)
+            next_level_index, next_left_levels
+        )
 
         curr_level_points = level_point(level)
         if curr_level_points + with_curr_level_point > without_curr_level_points:
-            return (curr_level_points + with_curr_level_point, [level, with_curr_level_result])
+            return (
+                curr_level_points + with_curr_level_point,
+                [level, with_curr_level_result],
+            )
 
         return (without_curr_level_points, without_curr_level_result)
 
@@ -180,30 +202,37 @@ def _merge_levels_best_way(levels: List[LevelCharacteristics],
 
 def __validate_stock_price_dataframe(stock_price: pd.DataFrame):
     expected_columns = {
-        'close': ['float64', 'float32'],
-        'high': ['float64', 'float32'],
-        'low': ['float64', 'float32'],
-        'open': ['float64', 'float32'],
-        'volume': ['int8', 'int16', 'int32', 'int64']
+        "close": ["float64", "float32"],
+        "high": ["float64", "float32"],
+        "low": ["float64", "float32"],
+        "open": ["float64", "float32"],
+        "volume": ["int8", "int16", "int32", "int64"],
     }
 
-    missing_columns = [col for col in expected_columns if col not in stock_price.columns]
-    mismatched_columns = [col for col in expected_columns if stock_price[col].dtype.name not in expected_columns[col]]
+    missing_columns = [
+        col for col in expected_columns if col not in stock_price.columns
+    ]
+    mismatched_columns = [
+        col
+        for col in expected_columns
+        if stock_price[col].dtype.name not in expected_columns[col]
+    ]
 
     if len(missing_columns) > 0:
         raise ValueError(f"Missing columns: {missing_columns}")
-    
+
     if len(mismatched_columns) > 0:
         raise ValueError(f"Columns with type mismatches: {mismatched_columns}")
 
 
 # Main function to calculate support and resistance levels
-def get_support_resistance_levels(stock_price: pd.DataFrame,
-               order: int,
-               level_merge_percentage: float,
-               max_level_number: Optional[int] = None,
-               level_point: Callable[[LevelCharacteristics], float] = None
-               ) -> Tuple[int, List[LevelCharacteristics]]:
+def get_support_resistance_levels(
+    stock_price: pd.DataFrame,
+    order: int,
+    level_merge_percentage: float,
+    max_level_number: Optional[int] = None,
+    level_point: Callable[[LevelCharacteristics], float] = None,
+) -> Tuple[int, List[LevelCharacteristics]]:
     """
     Calculate support and resistance levels based on input stock price data.
 
@@ -223,15 +252,27 @@ def get_support_resistance_levels(stock_price: pd.DataFrame,
     __validate_stock_price_dataframe(stock_price)
 
     if not isinstance(order, int) or order <= 0:
-        raise ValueError('The order parameter should a positive int.')
-    
-    if not (isinstance(level_merge_percentage, float) or isinstance(level_merge_percentage, int)) or level_merge_percentage <= 0:
-        raise ValueError('The level_merge_percentage parameter should be a positive float.')
-    
-    if max_level_number is not None and (not isinstance(max_level_number, int) or max_level_number <= 0):
-        raise ValueError('The max_level_number parameter should be a None or positive int.')
-    
+        raise ValueError("The order parameter should a positive int.")
+
+    if (
+        not (
+            isinstance(level_merge_percentage, float)
+            or isinstance(level_merge_percentage, int)
+        )
+        or level_merge_percentage <= 0
+    ):
+        raise ValueError(
+            "The level_merge_percentage parameter should be a positive float."
+        )
+
+    if max_level_number is not None and (
+        not isinstance(max_level_number, int) or max_level_number <= 0
+    ):
+        raise ValueError(
+            "The max_level_number parameter should be a None or positive int."
+        )
+
     levels = _get_level_characteristics(stock_price, order, level_merge_percentage)
 
-    _, best_levels = _merge_levels_best_way(levels,  max_level_number, level_point)
+    _, best_levels = _merge_levels_best_way(levels, max_level_number, level_point)
     return best_levels
