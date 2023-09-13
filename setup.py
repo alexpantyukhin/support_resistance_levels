fe1e48feca5caa5815bade0943c6043abdff811d
alexpantyukhin <apantykhin@gmail.com>
2023-09-13 16:25:12 +0400

    0.0.2 version

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="support_resistance",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    include_package_data=True,
     install_requires=[
         # List your dependencies here
         "pandas>=1.0.0",
