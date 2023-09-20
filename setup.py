cb44bfd528df2ceec73761d8510c1df1ee448488
alexpantyukhin <apantykhin@gmail.com>
2023-09-20 11:04:37 +0400

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="support_resistance",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     description='Lib for finding support resistance levels',
     author='Alexander Pantyukhin',
